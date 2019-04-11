f516ae578d02fa5e2adbe28f9442c6ab019ef5bb
Brian Bolnick <brianbolnick@gmail.com>
2019-04-10 21:10:52 -0600

    upgrade storybook and other deps

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,49 @@
-import { configure, addDecorator } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
+import { addParameters, configure, addDecorator } from '@storybook/react';
 import { withConsole } from "@storybook/addon-console";
+import { withInfo } from '@storybook/addon-info';
+import { create } from '@storybook/theming';
+
+const theme = create({
+  base: 'light',
+  colorSecondary: '#FE4969',
+
+  // UI
+  appBg: 'white',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: '#333333',
+  textInverseColor: 'white',
+
+  // Toolbar default and active colors
+  barTextColor: 'black',
+  barSelectedColor: '#4C76E0',
+  barBg: 'white',
+
+  // Form colors
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+	inputBorderRadius: 4,
+});
+
+addParameters({
+  options: {
+    addonPanelInRight: true,
+		theme: theme,
+    name: 'Hungry Hippo'
+  }
+});
 
 addDecorator(
-  withInfo({
-    inline: true,
-    maxPropArrayLength: 1,
-    maxPropObjectKeys: 60,
-    maxPropsIntoLine: 90,
-    maxPropStringLength: 300
-  })
+	withInfo({
+		inline: true,
+		maxPropArrayLength: 1,
+		maxPropObjectKeys: 60,
+		maxPropsIntoLine: 90,
+		maxPropStringLength: 300
+	})
 );
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
@@ -17,7 +51,7 @@ addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 const req = require.context("../src/components", true, /\.story\.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
