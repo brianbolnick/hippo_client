903632ead12df41bc9b4dd82aff123ca14ce3f16
Brian Bolnick <brianbolnick@gmail.com>
2019-04-28 15:31:55 -0600

    update styling for dropdown (wip)

--- src/components/common/Dropdown/Dropdown.js
+++ src/components/common/Dropdown/Dropdown.js
@@ -1,146 +1,98 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import Icon from '../Icon/Icon';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import Icon from "../Icon/Icon";
 
 const Container = styled.div`
-	user-select: none;
-	position: relative;
-	width: 222px;
+  user-select: none;
+  position: relative;
+  width: 222px;
 `;
 
 const Header = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	line-height: 38px;
-	border: 1px solid #dfdfdf;
-	border-radius: 3px;
-	cursor: default;
-	position: relative;
-	background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  line-height: 38px;
+  //border: 1px solid #dfdfdf;
+  border-radius: 3px;
+  cursor: default;
+  position: relative;
+  background-color: #fff;
 
-	span {
-		margin-right: 20px;
-	}
+  span {
+    margin-right: 20px;
+  }
 `;
 
 const HeaderTitle = styled.div`
-	font-weight: 300;
-	margin: 2px 20px;
-	margin-right: 30px;
+  font-weight: 300;
+  margin: 2px 20px;
+  margin-right: 30px;
 `;
 
 const List = styled.ul`
-	z-index: 10;
-	position: absolute;
-	width: 100%;
-	border: 1px solid #dfdfdf;
-	border-top: none;
-	border-bottom-right-radius: 3px;
-	border-bottom-left-radius: 3px;
-	background-color: #fff;
-	box-shadow: 0 2px 5px -1px #e8e8e8;
-	font-weight: 700;
-	padding: 15px 0;
-	max-height: 215px;
-	overflow-y: scroll;
+  z-index: 10;
+  position: absolute;
+  width: 100%;
+  border: 1px solid #dfdfdf;
+  border-radius: 3px;
+  background-color: #fff;
+  box-shadow: 0 2px 5px -1px #e8e8e8;
+  font-weight: 700;
+  padding: 15px 0;
+  max-height: 215px;
+  overflow-y: scroll;
+  margin: 0;
+  box-sizing: border-box;
 `;
 
 const ListItem = styled.li`
-	width: 100%;
-	font-size: 1.5rem;
-	padding: 8px 10px;
-	line-height: 1.6rem;
-	cursor: default;
-	display: inline-block;
-	white-space: nowrap;
-	text-overflow: ellipsis;
+  width: 100%;
+  font-size: 1.5rem;
+  padding: 8px 10px;
+  line-height: 1.6rem;
+  cursor: default;
+  display: inline-block;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 
-	&:hover {
-		color: #fff;
-		background-color: #ffcc01;
-	}
+  &:hover {
+    color: #fff;
+    background-color: #ffcc01;
+  }
 `;
 
-class Dropdown extends Component{
-	constructor(props){
-		super(props)
-		this.state = {
-			listOpen: false,
-			headerTitle: this.props.title
-		}
-		this.close = this.close.bind(this)
-	}
+const Dropdown = ({ defaultValue, list, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [displayValue, setDisplayValue] = useState("");
 
-	//componentDidUpdate(){
-		//const { listOpen } = this.state
-		//setTimeout(() => {
-			//if(listOpen){
-				//window.addEventListener('click', this.close)
-			//}
-			//else{
-				//window.removeEventListener('click', this.close)
-			//}
-		//}, 0)
-	//}
+  useEffect(() => {
+    if (!defaultValue && !displayValue) setDisplayValue(list[0].title);
+  });
 
-	//componentWillUnmount(){
-		//window.removeEventListener('click', this.close)
-	//}
+  const renderListItems = () =>
+    list.map(item => (
+      <ListItem key={item.id} onClick={onChange(item)}>
+        {item.title} {item.selected && <Icon name="checkCircle" />}
+      </ListItem>
+    ));
 
-	close(timeOut){
-		this.setState({
-			listOpen: false
-		})
-	}
-
-	selectItem(title, id, stateKey){
-		this.setState({
-			headerTitle: title,
-			listOpen: false
-		}, this.props.resetThenSet(id, stateKey))
-	}
-
-	toggleList(){
-		this.setState(prevState => ({
-			listOpen: !prevState.listOpen
-		}))
-	}
-
-	render(){
-
-		const { list, onChange } = this.props
-		const { listOpen, headerTitle } = this.state
-
-		return(
-			<Container>
-			<Header onClick={() => this.toggleList()}>
-			<HeaderTitle>{headerTitle}</HeaderTitle>
-			{listOpen
-				? <Icon name="chevronUp" />
-				:	<Icon name="chevronDown" />
-			}
-			</Header>
-			{listOpen && <List>
-				{list.map((item)=> (
-					<ListItem 
-						key={item.id} 
-						onClick={onChange(item)}
-					>
-					{item.title} {item.selected && <Icon name="checkCircle"/>}
-					</ListItem>
-				))}
-				</List>}
-			</Container>
-		)
-	}
-}
+  return (
+    <Container>
+      <Header onClick={() => setIsOpen(!isOpen)}>
+        <HeaderTitle>{displayValue}</HeaderTitle>
+        {isOpen ? <Icon name="chevronUp" /> : <Icon name="chevronDown" />}
+      </Header>
+      {isOpen && <List>{renderListItems()}</List>}
+    </Container>
+  );
+};
 
 Dropdown.propTypes = {
-	onChange: PropTypes.func.isRequired,
-	placeholder: PropTypes.string,
-	children: PropTypes.any
-}
+  onChange: PropTypes.func.isRequired,
+  defaultValue: PropTypes.string,
+  list: PropTypes.array.isRequired
+};
 
 export default Dropdown;
--- src/components/common/Dropdown/Dropdown.story.js
+++ src/components/common/Dropdown/Dropdown.story.js
@@ -3,33 +3,30 @@ import { storiesOf } from "@storybook/react";
 import Dropdown from "./Dropdown";
 import { action } from "@storybook/addon-actions";
 
-
 const list = [
-	    {
-				      id: 0,
-				      title: 'Apple',
-				      selected: false,
-				      key: 'fruit'
-				    },
-	    {
-				      id: 1,
-				      title: 'Orange',
-				      selected: false,
-				      key: 'fruit'
-				    },
-	    {
-				      id: 2,
-				      title: 'Strawberry',
-				      selected: false,
-				      key: 'fruit'
-				    }
-	  ]
-storiesOf("Dropdown", module)
-  .add("Default", () => (
-		<Dropdown
-			placeholder="Email Address"
-			onChange={action("change")}
-		list={list}
-		/>
-  ))
-
+  {
+    id: 0,
+    title: "Apple",
+    selected: false,
+    key: "fruit"
+  },
+  {
+    id: 1,
+    title: "Orange",
+    selected: false,
+    key: "fruit"
+  },
+  {
+    id: 2,
+    title: "Strawberry",
+    selected: false,
+    key: "fruit"
+  }
+];
+storiesOf("Dropdown", module).add("Default", () => (
+  <Dropdown
+    placeholder="Email Address"
+    onChange={action("change")}
+    list={list}
+  />
+));
