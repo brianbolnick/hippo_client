2a5a0ad861683e99e2f5342c24d8b9b58990a617
Brian Bolnick <brianbolnick@gmail.com>
2019-09-27 11:38:04 -0600

    wip on new styles

--- src/components/common/Recipe/RecipeCard.js
+++ src/components/common/Recipe/RecipeCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { colors } from "styles/css-variables";
-import { DISH_TYPE_ICON_MAP } from 'utils';
+import { DISH_TYPE_ICON_MAP } from "utils";
 import {
   Card,
   RecipeImage,
@@ -8,9 +8,9 @@ import {
   MetaData,
   Footer,
   Content,
-	LinkWrapper,
-	DishType,
-	DishTypeName,
+  LinkWrapper,
+  DishType,
+  DishTypeName
 } from "./Styles";
 import Icon from "components/common/Icon/Icon";
 import Difficulty from "components/common/Difficulty";
@@ -20,14 +20,14 @@ const RecipeCard = ({ data }) => {
   return (
     <Card>
       <LinkWrapper to={`/recipes/${data.id}`}>
-        <RecipeImage url={data.image_url || PlaceholderImage}>
-        </RecipeImage>
+        <RecipeImage url={data.image_url || PlaceholderImage}></RecipeImage>
         <Content>
           <Title>{data.title}</Title>
-					<MetaData>
-						<div>{data.category.name}</div>
-					</MetaData>
+          <MetaData>
+            <div>{data.category.name}</div>
+          </MetaData>
         </Content>
+        {/*
 				<Footer>
 						<Difficulty value={data.difficulty} />
 					<DishType>
@@ -40,6 +40,7 @@ const RecipeCard = ({ data }) => {
 					</DishType>
 
 		</Footer>
+		*/}
       </LinkWrapper>
     </Card>
   );
--- src/components/common/Recipe/Styles.js
+++ src/components/common/Recipe/Styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { colors, raleway, media } from "styles/css-variables";
+import { colors, avenir, media } from "styles/css-variables";
 import { Link } from "react-router-dom";
 
 export const LinkWrapper = styled(Link)`
@@ -26,25 +26,30 @@ export const RatingCount = styled.div`
 `;
 
 export const Card = styled.div`
-  width: 275px;
-  border-radius: 8px;
-  box-shadow: 0px 0px 10px 1px #2121213d;
-  background-color: white;
+  width: calc(20% - 32px);
   margin: 10px 16px;
   position: relative;
   overflow: hidden;
   color: ${colors.black};
   transition: 0.2s;
-	max-height: 340px;
+  max-height: 340px;
+  cursor: pointer;
 
-  ${media.phone`
-		width: 100%;
-		margin: 10px 25px;
+  ${media.smallDesktop`
+		width: calc(25% - 32px);
+	`};
+
+  ${media.tablet`
+		width: calc(50% - 32px);
 	`};
 
   &:hover {
-    box-shadow: 0px 0px 10px 5px #2121213d;
-    transition: 0.2s;
+    //box-shadow: 0px 0px 10px 5px #2121213d;
+    //transition: 0.2s;
+
+    ${RecipeImage} {
+      transform: scale(1.01);
+    }
   }
 `;
 
@@ -59,37 +64,38 @@ export const RecipeImage = styled.div`
   background-size: cover;
   background-position: center;
   position: relative;
+  border-radius: 8px;
 `;
 
 export const LevelContainer = styled.div`
-	display: flex	;
+  display: flex;
 `;
 
 export const LevelLabel = styled.div`
-	color: ${colors.mutedGray};
+  color: ${colors.mutedGray};
 `;
 export const Title = styled.div`
-  font-family: ${raleway};
-  font-size: 1.4rem;
+  font-family: ${avenir};
+  font-size: 1.1rem;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  margin-bottom: 16px;
+  font-weight: 600;
 `;
 
 export const MetaData = styled.div`
-	color: ${colors.mutedGray};
+  color: ${colors.mutedGray};
   text-transform: uppercase;
   font-size: 0.9rem;
   font-weight: 600;
   letter-spacing: 1px;
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 export const Footer = styled.div`
   width: 100%;
-	border-top: solid 1px ${colors.lightGray};
+  border-top: solid 1px ${colors.lightGray};
   height: 48px;
   padding: 8px;
   box-sizing: border-box;
@@ -100,15 +106,15 @@ export const Footer = styled.div`
 `;
 
 export const DishType = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 export const DishTypeName = styled.div`
-margin-left: 4px;
+  margin-left: 4px;
 `;
 
 export const Content = styled.div`
-  padding: 24px;
+  padding: 16px 0px;
   text-align: left;
 `;
