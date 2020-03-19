f06a202ec079c496d1d2ba0666d94fe74d597cda
Brian Bolnick <brianbolnick@gmail.com>
2020-03-18 21:46:51 -0600

    update

--- src/components/pages/MealPlans/ShoppingList/ShoppingList.js
+++ src/components/pages/MealPlans/ShoppingList/ShoppingList.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Layout from 'components/common/Layout/Layout';
+import { useMealPlanQuery } from '../hooks';
 
 const ShoppingList = () => {
+  const { id } = useParams();
+  const { data, loading } = useMealPlanQuery({ id: JSON.parse(id) });
+
+  const mealsMapping =
+    data.recipes &&
+    data.recipes.reduce((acc, rec) => {
+      acc[rec.title] = rec.rawIngredients;
+      return acc;
+    }, {});
+
+  const renderIngredients = () => {
+    return (
+      mealsMapping &&
+      Object.keys(mealsMapping).map(item => {
+        return (
+          <div>
+            <div>{item}</div>
+            <ul>
+              {mealsMapping[item].map(ing => (
+                <li key={`${item}-${ing}`}>{ing}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })
+    );
+  };
+
+  if (loading) return <div>loading</div>;
   return (
     <Layout>
-      <div>coming soon</div>
+      <div>COMING SOON!</div>
+      <ul>{renderIngredients()}</ul>
     </Layout>
   );
 };
--- src/components/pages/MealPlans/apollo/mealPlanQuery.js
+++ src/components/pages/MealPlans/apollo/mealPlanQuery.js
@@ -12,6 +12,7 @@ export default gql`
       recipes {
         id
         title
+        rawIngredients
       }
     }
   }
