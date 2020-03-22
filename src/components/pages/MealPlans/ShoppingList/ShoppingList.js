7d39a760873e7050dccd21e2c64ec6e0745e03cd
Brian Bolnick <brianbolnick@gmail.com>
2020-03-22 16:17:48 -0600

    list styles

--- src/components/pages/MealPlans/ShoppingList/ShoppingList.js
+++ src/components/pages/MealPlans/ShoppingList/ShoppingList.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import moment from 'moment';
 import { useParams } from 'react-router-dom';
+import { media, colors } from 'styles/css-variables';
 import Layout from 'components/common/Layout/Layout';
+import Checkbox from 'components/common/Checkbox';
 import { useMealPlanQuery } from '../hooks';
+
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 8px;
+`;
+const SubTitle = styled.div`
+  color: ${colors.darkGray};
+  margin-bottom: 32px;
+  font-size: 1.2em;
+`;
+
+const MealName = styled.div`
+  font-weight: 700;
+  margin-bottom: 12px;
+  font-size: 1.1em;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: flex-start;
+`;
+const MealWrapper = styled.div`
+  width: 33%;
+  margin-bottom: 24px;
+
+  ${media.tablet`
+		width: 100%;
+	`}
+`;
+const IngredientsContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+`;
+const Ingredient = styled.div`
+  display: flex;
+`;
+const IngredientName = styled.div``;
 
 const ShoppingList = () => {
   const { id } = useParams();
   const { data, loading } = useMealPlanQuery({ id: JSON.parse(id) });
+
+  const date = moment(data.insertedAt).format('MMMM Do, YYYY');
 
   const mealsMapping =
     data.recipes &&
@@ -19,14 +65,17 @@ const ShoppingList = () => {
       mealsMapping &&
       Object.keys(mealsMapping).map(item => {
         return (
-          <div>
-            <div>{item}</div>
-            <ul>
+          <MealWrapper>
+            <MealName>{item}</MealName>
+            <IngredientsContainer>
               {mealsMapping[item].map(ing => (
-                <li key={`${item}-${ing}`}>{ing}</li>
+                <Ingredient>
+                  <Checkbox onChange={() => console.log('check')} />
+                  <IngredientName key={`${item}-${ing}`}>{ing}</IngredientName>
+                </Ingredient>
               ))}
-            </ul>
-          </div>
+            </IngredientsContainer>
+          </MealWrapper>
         );
       })
     );
@@ -35,8 +84,9 @@ const ShoppingList = () => {
   if (loading) return <div>loading</div>;
   return (
     <Layout>
-      <div>COMING SOON!</div>
-      <ul>{renderIngredients()}</ul>
+      <Title>Shopping List</Title>
+      <SubTitle>Meal plan - {date}</SubTitle>
+      <Container>{renderIngredients()}</Container>
     </Layout>
   );
 };
